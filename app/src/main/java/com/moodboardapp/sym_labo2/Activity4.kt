ee75d16564f6ac7d4cbb26d8509315fe5e6be2ee
Stephane Bottin <stephane.bottin@heig-vd.ch>
2020-10-08 10:41:06 +0200

    added activities

---
+++ app/src/main/java/com/moodboardapp/sym_labo2/Activity4.kt
@@ -0,0 +1,11 @@
+package com.moodboardapp.sym_labo2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity4 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
