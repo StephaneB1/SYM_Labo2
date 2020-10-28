d698d4846ac29962c4a2ad38255ebab44c24a90d
Simon <simon.mattei@heig-vd.ch>
2020-10-28 18:52:36 +0100

    [WIP] Asynchrone txt

--- app/src/main/java/com/moodboardapp/sym_labo2/Activity1.kt
+++ app/src/main/java/com/moodboardapp/sym_labo2/Activity1.kt
@@ -4,8 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class Activity1 : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_1)
+
+        var scm = SymComManager()
+        scm.communicationEventListener = object : CommunicationEventListener {
+            override fun handleServerResponse(response: String) {
+                println(response)
+            }
+        }
+        scm.sendRequest(getString(R.string.url), "rest/txt")
     }
 }
--- app/src/main/java/com/moodboardapp/sym_labo2/SymComManager.kt
+++ app/src/main/java/com/moodboardapp/sym_labo2/SymComManager.kt
@@ -1,8 +1,35 @@
-package ch.heigvd.iict.sym.lab.comm
+package com.moodboardapp.sym_labo2
+
+import java.io.BufferedInputStream
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.OutputStream
+import java.net.HttpURLConnection
+import java.net.URL
 
 class SymComManager(var communicationEventListener: CommunicationEventListener? = null) {
 
     fun sendRequest(url: String, request: String) {
-		//TODO to implement
+        /*
+        val urlConnection = URL(url + request).openConnection() as HttpURLConnection
+        try {
+            readStream(BufferedInputStream(urlConnection.inputStream))
+        } finally {
+            urlConnection.disconnect()
+        }*/
     }
+
+    private fun readStream(inputStream: InputStream) {
+
+        val response: String
+        val reader = BufferedReader(inputStream.reader())
+        try {
+            response = reader.readText()
+        } finally {
+            reader.close()
+        }
+        communicationEventListener?.handleServerResponse(response)
+    }
+
+
 }
