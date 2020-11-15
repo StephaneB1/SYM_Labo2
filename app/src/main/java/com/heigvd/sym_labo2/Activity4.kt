9ca7b9635f30e4fa3865c1eedd5b3dc4070c6f82
Stephane Bottin <stephane.bottin@heig-vd.ch>
2020-11-15 21:32:51 +0100

    refactor

--- app/src/main/java/com/heigvd/sym_labo2/Activity4.kt
+++ app/src/main/java/com/heigvd/sym_labo2/Activity4.kt
@@ -2,10 +2,98 @@ package com.heigvd.sym_labo2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.heigvd.sym_labo2.comm.CommunicationEventListener
+import com.heigvd.sym_labo2.comm.SymComManager
 
 class Activity4 : AppCompatActivity() {
+
+    private lateinit var sendingText: TextView
+    private lateinit var receivedTime4GView: TextView
+    private lateinit var receivedTime2GView: TextView
+    private lateinit var receivedTimeComp4GView: TextView
+    private lateinit var receivedTimeComp2GView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_4)
+
+
+        sendingText = findViewById(R.id.sending_text)
+        receivedTime4GView = findViewById(R.id.received_time_4g)
+        receivedTime2GView = findViewById(R.id.received_time_2g)
+        receivedTimeComp4GView = findViewById(R.id.received_time_comp_4g)
+        receivedTimeComp2GView = findViewById(R.id.received_time_comp_2g)
+
+        // Timers
+        var start4g = 0L
+        var end4g = 0L
+        var start2g = 0L
+        var end2g = 0L
+        var startComp4g = 0L
+        var endComp4g = 0L
+        var startComp2g = 0L
+        var endComp2g = 0L
+
+        val mcm4G = SymComManager(object : CommunicationEventListener {
+            override fun handleServerResponse(response: String) {
+                end4g = System.currentTimeMillis()
+                runOnUiThread {
+                    receivedTime4GView.text = "Time with 4G : ${end4g - start4g}ms"
+                }
+            }
+        })
+
+        val mcm2G = SymComManager(object : CommunicationEventListener {
+            override fun handleServerResponse(response: String) {
+                end2g = System.currentTimeMillis()
+                runOnUiThread {
+                    receivedTime2GView.text = "Time with 2G : ${end2g - start2g}ms"
+                }
+            }
+        })
+
+        val mcmCompressed4G = SymComManager(object : CommunicationEventListener {
+            override fun handleServerResponse(response: String) {
+                endComp4g = System.currentTimeMillis()
+                runOnUiThread {
+                    receivedTimeComp4GView.text = "Time with 4G (compressed) : ${endComp4g - startComp4g}ms"
+                }
+            }
+        })
+
+        val mcmCompressed2G = SymComManager(object : CommunicationEventListener {
+            override fun handleServerResponse(response: String) {
+                endComp2g = System.currentTimeMillis()
+                runOnUiThread {
+                    receivedTimeComp2GView.text = "Time with 2G (compressed) : ${endComp2g - startComp2g}ms"
+                }
+            }
+        })
+
+        // Random string of 2000 characters for compression tests
+        val charPool = "abcdefghijklmnopqrstuvwxyz"
+        val randomString = (1..2000)
+            .map { i -> kotlin.random.Random.nextInt(0, charPool.length) }
+            .map(charPool::get)
+            .joinToString("")
+
+        sendingText.text = "Sending " + randomString.length + " characters"
+
+        // Send request with 4g connection
+        start4g = System.currentTimeMillis()
+        mcm4G.sendRequest(MainActivity.LAB_SERVER + "rest/txt",  randomString, "txt/plain")
+
+        // Send request with 2g connection
+        start2g = System.currentTimeMillis()
+        mcm2G.sendRequest(MainActivity.LAB_SERVER + "rest/txt",  randomString, "txt/plain", false)
+
+        // Send request with 4g connection with compressed content
+        startComp4g = System.currentTimeMillis()
+        mcmCompressed4G.sendRequest(MainActivity.LAB_SERVER + "rest/txt",  randomString, "txt/plain")
+
+        // Send request with 2g connection with compressed content
+        startComp2g = System.currentTimeMillis()
+        mcmCompressed2G.sendRequest(MainActivity.LAB_SERVER + "rest/txt",  randomString, "txt/plain", false)
     }
 }
--- app/src/main/java/com/heigvd/sym_labo2/comm/SymComManager.kt
+++ app/src/main/java/com/heigvd/sym_labo2/comm/SymComManager.kt
@@ -8,40 +8,81 @@ import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
 import java.util.stream.Collectors
+import java.util.zip.Deflater
 
 class SymComManager(var communicationEventListener: CommunicationEventListener? = null) {
 
 
-    fun sendRequest(url: String, request: String, contentType: String) {
+    fun sendRequest(
+        url: String,
+        request: String,
+        contentType: String,
+        fastConnection: Boolean = true,
+        compressed: Boolean = false
+    ) {
+
+        var myRequest = request
         // Start http thread (so it's not on the main)
-        var httpHandler = HandlerThread("httpThread")
+        val httpHandler = HandlerThread("httpThread")
         httpHandler.start()
         // Start handler for the http request
         Handler(httpHandler.looper).postDelayed({
             val mURL = URL(url)
 
             with(mURL.openConnection() as HttpURLConnection) {
+                // Settings
                 setRequestProperty("Content-Type", contentType)
                 doOutput = true // indicates POST method
-                //doInput= true
-                Log.d(TAG, "sendRequest: Sending : " + request)
-                outputStream.write(request.toByteArray())
-
-                // Handle response
-                val br: BufferedReader
-                if (responseCode in 100..399) {
-                    br = BufferedReader(InputStreamReader(inputStream));
-                } else {
-                    br = BufferedReader(InputStreamReader(errorStream));
+                if (!fastConnection)
+                    setRequestProperty("X-Network", "CSD")
+                if (compressed) {
+                    setRequestProperty("X-Content-Encoding", "deflate")
+                    myRequest = compressContent(request)
                 }
 
-                val responseBody = br.lines().collect(Collectors.joining())
+                Log.d(TAG, "sendRequest: Sending : " + myRequest)
+
+                outputStream.write(myRequest.toByteArray())
+
+                // Handle response
+                val br: BufferedReader = if (responseCode in 100..399) {
+                    BufferedReader(InputStreamReader(inputStream));
+                } else {
+                    BufferedReader(InputStreamReader(errorStream));
+                }
+
+                val responseBody = if (compressed) {
+                    br.lines().collect(Collectors.joining())
+                } else {
+                    readCompressedResponse(br.lines().collect(Collectors.joining()))
+                }
 
                 Log.d(TAG, "sendRequest: Received : " + responseBody)
 
                 communicationEventListener?.handleServerResponse(responseBody)
             }
         }, 0)
+    }
+
+    // No time... :(
+    fun readCompressedResponse(response: String) : String {
+        return ""
+    }
+
+    private fun compressContent(content: String) : String {
+        // Encode a String into bytes
+        val input = content.toByteArray(charset("UTF-8"))
+
+        // Compress the bytes
+        val output = ByteArray(100)
+        val compresser = Deflater()
+        compresser.setInput(input)
+        compresser.finish()
+
+        val compressedDataLength = compresser.deflate(output)
+        compresser.end()
+
+        return output.toString()
     }
 
     companion object {
